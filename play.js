51c3e3c2fcfea31ddf0ef1cf00d5c030d5df6394
Natalia Yuzhakova <natalia.yuzhakova@gmail.com>
2020-07-01 20:30:34 +0000

    client setup

--- play.js
+++ play.js
@@ -4,19 +4,19 @@ const net = require('net');
  * Establishes connection with the game server
  */
 const connect = function() {
-  const conn = net.createConnection({ 
+  const conn = net.createConnection({
     host: '135.23.222.148',
     port: 50541
   });
   // interpret incoming data as text
-  conn.setEncoding('utf8'); 
+  conn.setEncoding('utf8');
 
   conn.on('data', (data) => {
-    console.log('I gat dat from serva >>', data)
-  })
+    console.log('I gat dat from serva >>', data);
+  });
 
   return conn;
-}
+};
 
 console.log('Connecting ...');
 connect();
