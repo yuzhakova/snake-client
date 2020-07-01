b7f2b56456f3063b56a72a0059a471974d50a7b9
Natalia Yuzhakova <natalia.yuzhakova@gmail.com>
2020-07-01 20:50:15 +0000

    Moved the connect function of play.js file to client.js file, where it's exported via module

---
+++ client.js
@@ -0,0 +1,21 @@
+const net = require('net');
+
+/**
+ * Establishes connection with the game server
+ */
+const connect = function() {
+  const conn = net.createConnection({
+    host: '135.23.222.148',
+    port: 50541
+  });
+  // interpret incoming data as text
+  conn.setEncoding('utf8');
+
+  conn.on('data', (data) => {
+    console.log('I gat dat from serva >>', data);
+  });
+
+  return conn;
+};
+
+module.exports = { connect };
--- play.js
+++ play.js
@@ -1,9 +1,19 @@
-const net = require('net');
+const { connect } = require('./client');
+console.log('Connecting ...');
+connect();
+
+// step 1: added this:
+// conn.on('data', (data) => {
+//   console.log('I gat dat from serva >>', data)
+// })
+
+// step 2: moved this to another JS file: client.js + added this line in the bottom: module.exports = { connect };
+/*const net = require('net');
 
 /**
  * Establishes connection with the game server
  */
-const connect = function() {
+/*const connect = function() {
   const conn = net.createConnection({
     host: '135.23.222.148',
     port: 50541
@@ -18,10 +28,4 @@ const connect = function() {
   return conn;
 };
 
-console.log('Connecting ...');
-connect();
-
-// step 1: added this:
-// conn.on('data', (data) => {
-//   console.log('I gat dat from serva >>', data)
-// })
+module.exports = { connect };*/
