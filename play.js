d75cd96f7bba8fa2c97f4aa026e10873fa7d21a4
Natalia Yuzhakova <natalia.yuzhakova@gmail.com>
2020-07-01 20:28:21 +0000

    client setup

---
+++ play.js
@@ -0,0 +1,27 @@
+const net = require('net');
+
+/**
+ * Establishes connection with the game server
+ */
+const connect = function() {
+  const conn = net.createConnection({ 
+    host: '135.23.222.148',
+    port: 50541
+  });
+  // interpret incoming data as text
+  conn.setEncoding('utf8'); 
+
+  conn.on('data', (data) => {
+    console.log('I gat dat from serva >>', data)
+  })
+
+  return conn;
+}
+
+console.log('Connecting ...');
+connect();
+
+// step 1: added this:
+// conn.on('data', (data) => {
+//   console.log('I gat dat from serva >>', data)
+// })
---
+++ playStarterScript.js
@@ -0,0 +1,18 @@
+const net = require('net');
+
+/**
+ * Establishes connection with the game server
+ */
+const connect = function() {
+  const conn = net.createConnection({ 
+    host: '135.23.222.148',
+    port: 50541
+  });
+  // interpret incoming data as text
+  conn.setEncoding('utf8'); 
+
+  return conn;
+}
+
+console.log('Connecting ...');
+connect();
